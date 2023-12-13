5ca35b7253ab55a8213155390bf02e5a4e0041bf
Amitav <amitav@pearlthoughts.com>
2023-12-13 16:20:25 +0530

    fixed upload url

--- medusa-config.js
+++ medusa-config.js
@@ -41,7 +41,7 @@ const plugins = [
     resolve: `@medusajs/file-local`,
     options: {
       upload_dir: "uploads",
-      backend_url: process.envMEDUSA_BACKEND_URL,
+      backend_url: process.env.MEDUSA_BACKEND_URL,
     },
   },
   {
